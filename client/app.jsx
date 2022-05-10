3b999679a5c4286f6f82782622fb41fd22402018
lkhuu96 <khuu96@gmail.com>
2022-05-10 16:33:38 -0700

    added nodemon.  created form

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
 class App extends React.Component {
+
+
+  form() {
+    return (
+      <form className='row w-100' action='submit'>
+        <label className='col-full' htmlFor="prompt">Enter prompt</label>
+        <textarea className='col-full' name="prompt" id="prompt" cols="30" rows="10"></textarea>
+        <input type="submit" />
+      </form>
+    )
+  }
   render() {
     return (
-      <div>
-        test
+      <div className='container'>
+        <h1>Fun with AI</h1>
+        <div className="row w-100">
+          {this.form()}
+        </div>
+        <h2>Response</h2>
       </div>
     )
   }
